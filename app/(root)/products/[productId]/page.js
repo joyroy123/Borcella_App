ac98ffdd86989c95ce0766b4179a49c93f57432b
joyroy123 <joyroy0349@gmail.com>
2025-01-07 16:09:52 +0600

    Fetch Related Products by Collection

--- app/(root)/products/[productId]/page.js
+++ app/(root)/products/[productId]/page.js
@@ -6,7 +6,7 @@ import { getProductDetails, getRelatedProducts } from "@/lib/actions/actions";
 const ProductDetails = async ({ params }) => {
   const { productId } = await params;
   const productDetails = await getProductDetails(productId);
-  // const relatedProducts = await getRelatedProducts(productId);
+  const relatedProducts = await getRelatedProducts(productId);
 
   return (
     <>
@@ -15,14 +15,15 @@ const ProductDetails = async ({ params }) => {
         <ProductInfo productInfo={productDetails} />
       </div>
 
-      {/* <div className="flex flex-col items-center px-10 py-5 max-md:px-3">
+      <div className="flex flex-col items-center px-10 py-5 max-md:px-3">
         <p className="text-heading3-bold">Related Products</p>
+        {relatedProducts.length === 0 && <p className="text-red-500 font-semibold mt-5">No Related Products!</p>}
         <div className="flex flex-wrap gap-16 mx-auto mt-8">
           {relatedProducts?.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
         </div>
-      </div> */}
+      </div>
     </>
   );
 };
